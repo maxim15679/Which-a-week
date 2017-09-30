07392d1a9a0805653806381f82f9b655845b6770
user <user@Mac-Pro-user.local>
2017-09-30 15:01:07 +0300

    Algorithm fixed for FRIDAY bug

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,14 +1,26 @@
 var a = new Date();
 if (a.getMonth() < 8) {
-  var b = new Date(a.getFullYear() - 1, 8)
+  var b = new Date(a.getFullYear() - 1, 8);  
 } else {
   var b = new Date(a.getFullYear(), 8)
 }
-var c = a-b;
-c = Math.ceil(c / 1000 / 60 / 60 / 24 / 7);
-if (c % 2 == 0) {
+if (b.getUTCDay() != 0) {
+  b = new Date(b.getFullYear(), 8, -b.getUTCDay() + 1);
+}
+var i = 0;
+var y = b.getTime()
+while (y <= a.getTime()) {
+y += 1000 * 60 * 60 * 24 * 7;
+i++;
+}
+
+
+
+//var c = a-b;
+//c = Math.ceil(c / 1000 / 60 / 60 / 24 / 7);
+if (i % 2 == 0) {
   $('.oddity').html('Четная');
 } else {
   $('.oddity').html('<span style="color:red">НЕ</span>четная');
 }
-$('.test').html(c);
+$('.test').html(i);
